9538503fae1ab7867b6ee2cd06614cefef5ba151
Nikita <xolodov00@yandex.ru>
2023-08-15 12:40:11 +0300

    fix5

--- js/Parrots.js
+++ js/Parrots.js
@@ -7,16 +7,16 @@ canvas.width = width
 canvas.height = height
 let radius = 50
 
-var frameUrls = ["./../img/0.png",
-	"./../img/1.png",
-	"./../img/2.png",
-	"./../img/3.png",
-	"./../img/4.png",
-	"./../img/5.png",
-	"./../img/6.png",
-	"./../img/7.png",
-	"./../img/8.png",
-	"./../img/9.png"]
+var frameUrls = ["./img/0.png",
+	"./img/1.png",
+	"./img/2.png",
+	"./img/3.png",
+	"./img/4.png",
+	"./img/5.png",
+	"./img/6.png",
+	"./img/7.png",
+	"./img/8.png",
+	"./img/9.png"]
 
 var parrot = {
 	frames: [],
